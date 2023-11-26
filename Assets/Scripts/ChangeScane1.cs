fabdba03b261eaf6994ca3312a7916a5a8cde6ee
none123 <76775605+TIM-ai-coder@users.noreply.github.com>
2023-11-27 02:43:33 +0900

    대학본부(화재_탈출) , 디지털관(물_누전위험) 맵 추가
    디관맵은 누수로 물이 가득찬 상황 구현해서,
    미션하면 물(cube)가 줄어들게 하면 될 것 같고
    대학본부도 화재상황 만들어놔서
    간단한 미션 깨고 탈출 구현하면 될듯?합네다

---
+++ Assets/Scripts/ChangeScane1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class ChangeScane1 : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onCollide;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return; //Player가 아니면 실행 하지 않음
+        onCollide?.Invoke();
+        Invoke("nextScene", 1);
+    }
+
+
+    void nextScene()
+    {
+        Scene currScene = SceneManager.GetActiveScene(); //현재 씬 정보 획득
+        switch (currScene.name)
+        {
+            case "MainScene":
+                SceneManager.LoadScene("테크노Scene"); break;
+            case "테크노Scene":
+                SceneManager.LoadScene("MainScene"); break;
+        }
+    }
+
+}
